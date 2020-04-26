844317a9b82d8a176060625d6c838a9e54159087
takashi <tk19490612@gmail.com>
2020-04-26 13:49:06 +0900

    15の倍数ならFizzBuzzと出力するようにした 条件関係に依存しない書き方大事

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,11 @@
 number = int(input("1つの自然数を入れてね: "))
 
-if number % 3 == 0:
-    output = "fizz"
+if number % 15 == 0:
+    output = "Fizz Buzz"
 elif number % 5 == 0:
     output = "Buzz"
+elif number % 3 == 0:
+    output = "Fizz"
 else:
     output = str(number)
 
